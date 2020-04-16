0c5f846b0e2b813f6995d0dc163bc11945c7905e
Samuel Costa <samu.ks@outlook.com>
2020-04-16 05:13:49 -0300

    creating dynamic content, gathering the content in data.js and passing it to the front through the backend

---
+++ semana01/modulo3/data.js
@@ -0,0 +1,36 @@
+module.exports = [
+    {
+        id: "cprMYC8PCVY",
+        title: "WorkshopDev - Episódio 01 | Mayk Brito",
+        duration: "1h 15min",
+        price: "Free"
+    },
+
+    {
+        id: "cprMYC8PCVY",
+        title: "WorkshopDev - Episódio 02 | Mayk Brito",
+        duration: "1h 54min",
+        price: "Free"
+    },
+
+    {
+        id: "sF2-77fUzQo",
+        title: "WorkshopDev - Episódio 03 | Mayk Brito",
+        duration: "43min",
+        price: "Free"
+    },
+
+    {
+        id: "KnBGxNHOLZQ",
+        title: "WorkshopDev - Episódio 04 | Mayk Brito",
+        duration: "1h 50min",
+        price: "Free"
+    },
+
+    {
+        id: "wkjyNfBjT74",
+        title: "WorkshopDev - Episódio 05 | Mayk Brito",
+        duration: "1h 09min",
+        price: "Free"
+    }
+]
--- semana01/modulo3/server.js
+++ semana01/modulo3/server.js
@@ -2,23 +2,35 @@ const express = require('express')
 const nunjunks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjunks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false
 })
 
 server.get("/", function(req, res){
-    return res.render("about")
+    const about = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/12562944?s=460&u=27b4d592488c0af727db907f0faee4b8717fae88&v=4",
+        name: "Samuel Costa",
+        role: "Full Stack Developer",
+        description: "Estudante de Tecnologia da Informação",
+        links: [
+            {name: "GitHub", url: "https://github.com/samukcosta"},
+            {name: "Linkedin", url: "https://www.linkedin.com/in/samuel-costa-a58b80181/"},
+            {name: "Email", url:"mailto:samu.ks@outlook.com"}
+        ]
+    }
+    return res.render("about", {about})
 })
 
 server.get("/portfolio", function(req, res){
-    return res.render("portfolio")
+
+    return res.render("portfolio", {items: videos})
 })
-
-
 
 server.listen(5000, function(){
     console.log("server is running")
