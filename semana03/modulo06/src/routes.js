3dd62b61f785c65d359de57e118802eacfb88dd1
Samuel Costa <samu.ks@outlook.com>
2020-06-19 00:13:35 -0300

    route to product recommendations

--- semana03/modulo06/src/routes.js
+++ semana03/modulo06/src/routes.js
@@ -8,10 +8,11 @@ routes.get("/", function(req, res){
 })
 
 routes.get("/products/create", ProductControler.create)
+routes.get("/products/:id", ProductControler.show)
 routes.get("/products/:id/edit", ProductControler.edit)
 
 routes.post("/products", multer.array("photos", 6), ProductControler.post)
-routes.put("/products", ProductControler.put)
+routes.put("/products", multer.array("photos", 6), ProductControler.put)
 routes.delete("/products", ProductControler.delete)
 
 routes.get("/ads/create", function(req, res){
