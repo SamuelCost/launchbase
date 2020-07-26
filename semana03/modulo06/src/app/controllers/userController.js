513a0cbe08db3b5e1356d952ecba07cb6d6d308a
Samuel Costa <samu.ks@outlook.com>
2020-07-26 06:45:50 -0300

    controller users

--- semana03/modulo06/src/app/controllers/userController.js
+++ semana03/modulo06/src/app/controllers/userController.js
@@ -1,18 +1,51 @@
 const User = require('../models/user')
+const { formatCep, formatCpfCnpj} = require('../../lib/utils')
 
 module.exports = {
     registerForm(req, res){
         return res.render("users/register")
     },
-    show(req, res) {
-        return res.send('ok, cadastrado!')
+    async show(req, res) {
+        const {user} = req
+
+        user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+        user.cep = formatCep(user.cep)
+
+        return res.render('users/index', {user})
     },
     async post(req, res) {
 
         const userID = await User.create(req.body)
-        console.log(userID)
+
+        req.session.userID = userID
 
         return res.redirect('/users')
         
+    },
+    async update(req, res) {
+        try {
+
+            const {user} = req
+
+            let {name, email, cpf_cnpj, cep, address} = req.body
+
+            cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+            cep = cep.replace(/\D/g, "")
+
+            await User.update(user.id, {
+                name, email, cpf_cnpj, cep, address
+            })
+
+            return res.render("users/index", {
+                user: req.body,
+                success: "Conta atualizada com sucesso!"
+            })
+
+        } catch (err) {
+            console.error(err)
+            return res.render("users/index", {
+                error: "Algum erro aconteceu!"
+            })
+        }
     }
 }
