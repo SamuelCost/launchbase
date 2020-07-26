6561dd455f9b15fa12486ab9b4939bba80deb019
Samuel Costa <samu.ks@outlook.com>
2020-07-26 06:36:27 -0300

    introduction of login, logout, registration and validation routes.

--- semana03/modulo06/src/routes/users.js
+++ semana03/modulo06/src/routes/users.js
@@ -3,23 +3,28 @@ const routes = express.Router()
 
 const SessionController = require("../app/controllers/sessionController")
 const UserController = require("../app/controllers/userController")
-const Validator = require("../app/validators/user")
+const UserValidator = require("../app/validators/user")
+const SessionValidator = require("../app/validators/session")
 
-/*routes.get('/login', SessionController.loginForm)
-routes.post('/login', SessionController.login)
-routes.post('/logout', SessionController.logout)
+/*
 
 routes.get('/forgot-password', SessionController.forgotForm)
 routes.get('/password-reset', SessionController.resetForm)
 routes.post('/forgot-password', SessionController.forgot)
 routes.post('/password-reset', SessionController.reset)
 
-routes.put('/', UserController.update)
 routes.delete('/', UserController.delete) */
 
+routes.get('/login', SessionController.loginForm)
+routes.post('/login', SessionValidator.login, SessionController.login)
+routes.post('/logout', SessionController.logout)
+
 routes.get('/register', UserController.registerForm)
-routes.post('/register', Validator.post, UserController.post)
-routes.get('/', UserController.show)
+routes.get('/', UserValidator.show, UserController.show)
+
+routes.post('/register', UserValidator.post, UserController.post)
+routes.put('/', UserValidator.update, UserController.update)
+
 
 
 
