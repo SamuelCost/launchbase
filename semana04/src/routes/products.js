2289fd531e3edf8bccd4ceba00116930d5f890f3
Samuel Costa <samu.ks@outlook.com>
2020-09-02 22:48:42 -0300

    add validation routes

--- semana04/src/routes/products.js
+++ semana04/src/routes/products.js
@@ -4,6 +4,8 @@ const routes = express.Router()
 const multer = require("../app/middlewares/multer")
 const ProductControler = require("../app/controllers/productController")
 const SearchControler = require("../app/controllers/searchController")
+const Validator = require('../app/validators/product')
+
 const {onlyUsers} = require('../app/middlewares/session')
 
 
@@ -13,8 +15,8 @@ routes.get("/create", onlyUsers, ProductControler.create)
 routes.get("/:id", ProductControler.show)
 routes.get("/:id/edit", onlyUsers, ProductControler.edit)
 
-routes.post("/", onlyUsers, multer.array("photos", 6), ProductControler.post)
-routes.put("/", onlyUsers, multer.array("photos", 6), ProductControler.put)
+routes.post("/", onlyUsers, multer.array("photos", 6), Validator.post, ProductControler.post)
+routes.put("/", onlyUsers, multer.array("photos", 6), Validator.put, ProductControler.put)
 routes.delete("/", onlyUsers, ProductControler.delete)
 
 module.exports = routes
